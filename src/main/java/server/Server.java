dda6b3c7357d79b4de3e7e953beca172129b6f28
7Adrian <7adrian.mail@gmail.com>
2019-05-29 16:10:56 +0200

    Server

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,10 +1,60 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+	private ServerSocket serverSocket;
+	private Socket clientSocket;
+	private PrintWriter out;
+	private BufferedReader in;
+
+	public void start(int port) {
+		String greeting = null;
+		try {
+
+			serverSocket = new ServerSocket(port);
+			clientSocket = serverSocket.accept();
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			greeting = in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if ("hello server".equals(greeting)) {
+			out.println("hello client");
+		} else {
+			out.println("unrecognised greeting");
+		}
+	}
+
+	public void stop() {
+		try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		out.close();
+		try {
+			clientSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Server server = new Server();
+		server.start(6666);
 	}
 
 }
