ec75e6f01a4fe1b440fc27ea31ac9091c95c09bd
01142546 <01142546@K204-63.liwe.ee.pw.edu.pl>
2019-05-29 17:47:40 +0200

    window

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -9,30 +9,28 @@ import java.util.Scanner;
 import graphics.Window;
 
 public class Client {
-	private Socket socket;
+	public Socket socket;
 	private Scanner scanner;
 
-	private Client(InetAddress serverAddress, int serverPort) throws Exception {
+	public Client(InetAddress serverAddress, int serverPort) throws Exception {
 		this.socket = new Socket(serverAddress, serverPort);
 		this.scanner = new Scanner(System.in);
 	}
 
-	private void start() throws IOException {
-		String input;
-		while (true) {
-			input = scanner.nextLine();
-			PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
-			out.println(input);
-			out.flush();
-		}
+	public void start() throws IOException {
+		
 	}
 
 	public static void main(String[] args) throws Exception {
 		new Window();
 		
-		Client client = new Client(InetAddress.getByName("145.239.82.150"), 6666);
-
-		System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
-		client.start();
+		
+	}
+	
+	public void send(String message) throws IOException
+	{
+		PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+		out.println(message);
+		out.flush();
 	}
 }
--- src/main/java/graphics/Window.java
+++ src/main/java/graphics/Window.java
@@ -1,24 +1,73 @@
 package graphics;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-public class Window extends JFrame{
-	
+import client.Client;
 
+public class Window extends JFrame implements ActionListener{
+
+	JTextField field = new JTextField();
+	JButton button = new JButton("send");
+	String message = new String();
+	Client client;
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public Window()
-	{
+	public Window() throws UnknownHostException, Exception {
+		
+		client = new Client(InetAddress.getByName("145.239.82.150"), 6666);
+
+		System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+		try {
+			client.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setBounds(0, 0, 800, 600);
 		setVisible(true);
+
+		field.setBounds(200, 200, 200, 50);
+		add(field);
 		
+		button.setBounds(450, 200, 50, 50);
+		button.addActionListener(this);
+		add(button);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==button) {
+			 message=field.getText();
+			 try {
+				client.send(message);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
 		
+	}
+
+	public String getMessage()
+	{
+		return message;
 	}
 	
 }
