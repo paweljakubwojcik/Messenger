f4a8df58370503936114b49fe1061486ed3f8238
Pafeu <Pafeu@LAPTOP-I0IJ9E63>
2019-05-29 16:10:39 +0200

    client

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,10 +1,54 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
 
+	private Socket clientSocket;
+	private PrintWriter out;
+	private BufferedReader in;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	}
+
+	public void startConnection(String ip, int port) {
+		try {
+			clientSocket = new Socket(ip, port);
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
+	public String sendMessage(String msg) {
+		out.println(msg);
+		String resp;
+		try {
+			resp = in.readLine();
+			return resp;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void stopConnection() {
+		try {
+			in.close();
+			out.close();
+			clientSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
